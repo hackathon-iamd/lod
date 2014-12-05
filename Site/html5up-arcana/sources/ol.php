cec6fdb8eca4e313aef24d05ab1688884f0a54e5
Ondine LAMY <ondine.lamy@telecomnancy.net>
2014-12-05 01:59:15 +0100

    Add text of my presentation and the presentation of TN

--- Site/html5up-arcana/sources/ol.php
+++ Site/html5up-arcana/sources/ol.php
@@ -16,7 +16,21 @@
 								
 								<h3>What about her ?</h3>
 								<p>
-									""
+									"Après un baccalauréat S je suis allée en classe préparatoire MPSI (Maths-Physique 
+									et sciences de l'ingénieur) puis MP au Centre International de Valbonne à Sophia 
+									Antipolis. Après avoir réussi les concours de la banque Mines-Télécom, j'ai 
+									intégrée TELECOM Nancy où je suis actuellement en 3ème année avec 
+									approfondissement Ingénierie et Applications des Masses de Données (IAMD). <br/>
+									Je suis intéressée par la gestion de bases de données, le développement Java, C/C++ 
+									et le développement web. Je suis curieuse à propos du domaine de l'intelligence 
+									artificielle. <br/>
+									Que ce soit à travers l'école ou en extra-scolaire j'ai pu participer à deux 
+									hackathons (un d'une journée et celui-là d'une semaine) et à deux 
+									<a href="http://www.nuitdelinfo.com/">nuits de l'info</a> (celle de 2013 et celle de 
+									2014). 
+									De plus j'ai été responsable du Maid Café, un salon de thé typique au Japon, 
+									à <a href="http://www.animest.net/">Anim'Est</a> 2013 et trésorière au bureau des élèves du 
+									<a href="http://www.ceten.fr/">CETEN</a> en 2014. "
 								</p>
 								<p>She's on <a href="http://fr.linkedin.com/in/ondinelamy/">linkedin</a>.</p>
 								<p class="return">Retour to the <a href="index.php?action=team">team</a>.</p>
